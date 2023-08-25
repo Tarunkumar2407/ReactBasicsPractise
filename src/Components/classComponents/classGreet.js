ee2d3d43eca10b8fc4bb2fad3397cbcdd92ded95
Tarun Kumar <tarunkumar2407@gmail.com>
2023-08-25 11:57:12 +0530

    initial commit

---
+++ src/Components/classComponents/classGreet.js
@@ -0,0 +1,21 @@
+// import React, { Component } from "react";
+
+// class GreetClass extends Component {
+//     render () {
+//         return (
+//             <h1>Hello from Class Component</h1>
+//         )
+//     }
+// }
+
+import React from "react"
+
+class GreetClass extends React.Component {
+    render () {
+        return (
+            <h1>Hello from React.Component</h1>
+        )
+    }
+}
+
+export default GreetClass
